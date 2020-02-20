c7e1ed597f9092545ffba9bc7b39b543747c9a21
zhuge <zhuge.abc@gmail.com>
2020-02-20 10:51:24 +0800

    update

--- metascape/index.js
+++ metascape/index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://metascape.org/gp/index.html#/main/step1');
+  await page.goto('http://www.google.com');
   await page.pdf({path: 'output/hn.pdf', format: 'A4'});
  
   await browser.close();
