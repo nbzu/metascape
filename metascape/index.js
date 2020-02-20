577c6705d0fdb85431e69711a624d9bc7f13c077
zhuge <zhuge.abc@gmail.com>
2020-02-20 19:21:07 +0800

    update

--- metascape/index.js
+++ metascape/index.js
@@ -1,10 +1,62 @@
 const puppeteer = require('puppeteer');
- 
+
+let page = null;
+let input = null;
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false});
   const page = await browser.newPage();
-  await page.goto('http://www.google.com');
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
- 
-  await browser.close();
+  await page.goto('http://metascape.org/gp/index.html#/main/step1');
+
+  await page.waitForSelector('#eq1').then(
+        async el=>{
+            await page.evaluate(() => {
+                document.querySelector("#eq1").click();
+            })
+        }
+    );
+
+  await page.waitForXPath('//input[@type="file"]').then(
+        async input=>{
+            await input.uploadFile('input/input.csv')
+            await input.evaluate(upload => {
+                upload.dispatchEvent(new Event('change', { bubbles: true }))
+            });
+        }
+    );
+
+  await page.waitForSelector('body > div:nth-child(3) > div > div:nth-child(1) > div.col-md-7.ng-scope > div > table > tbody > tr:nth-child(2) > td.info > div > table > tbody > tr:nth-child(1) > td:nth-child(2)',{visible: true}).then(
+        async select=>{
+            await select.click()
+        }
+    );
+  await page.waitForSelector('body > div.k-animation-container > div > div.k-list-scroller > ul > li:nth-child(2)').then(
+        async select=>{
+            await select.click()
+        }
+    );
+  await page.waitForSelector('#button-div > button:nth-child(1)').then(
+        async select=>{
+            await select.click()
+        }
+    );
+
+  await page.waitFor(60*5*1000)
+  await page.waitForSelector('body > div:nth-child(3) > div > div:nth-child(1) > div.col-md-7.ng-scope > div > table > tbody > tr:nth-child(3) > td.info > div:nth-child(2) > div:nth-child(2) > button',{visible: true}).then(
+        async select=>{
+            await select.click()
+        }
+    );
+  await page.waitFor(10*1*1000)
+  const pages = await browser.pages()
+  const reportfinal = pages[pages.length - 1];
+
+  await reportfinal._client.send('Page.setDownloadBehavior', {
+        behavior: 'allow', 
+        downloadPath: 'output'
+    })
+
+    await (await reportfinal.waitForSelector('body > div:nth-child(3) > div > div > div > div:nth-child(3) > a:nth-child(3)')).click();
+    await reportfinal.waitFor(10*1000)
+  
+    await browser.close()
 })();
